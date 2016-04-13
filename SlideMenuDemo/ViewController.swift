b6cf1f2e810a479b22f7d0ace6fa3c57c83ac017
Eagle <wei.lee460@gmail.com>
2016-04-13 20:32:20 +0800

    Add project files. 2016-04-13

---
+++ SlideMenuDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SlideMenuDemo
+//
+//  Created by ying on 16/4/13.
+//  Copyright © 2016年 ying. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
